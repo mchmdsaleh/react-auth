171eff693983afbf7514929a7eb75404aeafeb89
mchmdsaleh26 <mochammadsaleh26@gmail.com>
2023-12-22 07:18:15 +0700

    fix routes

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,18 @@ import About from "./pages/About";
 import Profile from "./pages/Profile";
 import Signin from "./pages/Signin";
 import SignUp from "./pages/SignUp";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <BrowserRouter>
+      <Navbar/>
       <Routes>
-        <Route path="/" element={<Home />}>
-          <Route path="about" element={<About />} />
-          <Route path="profile" element={<Profile />} />
-          <Route path="signin" element={<Signin />} />
-          <Route path="signup" element={<SignUp />} />
-        </Route>
+        <Route path="/" element={<Home />}/>
+        <Route path="about" element={<About />} />
+        <Route path="profile" element={<Profile />} />
+        <Route path="signin" element={<Signin />} />
+        <Route path="signup" element={<SignUp />} />
       </Routes>
     </BrowserRouter>
   );
